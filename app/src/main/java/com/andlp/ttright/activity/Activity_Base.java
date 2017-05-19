baf9434cd50bce16467527b2b22475382bc98692
liupeng110 <717219917@qq.com>
2017-05-19 16:43:32 +0800

    0.0

--- app/src/main/java/com/andlp/ttright/activity/Activity_Base.java
+++ app/src/main/java/com/andlp/ttright/activity/Activity_Base.java
@@ -2,6 +2,7 @@ package com.andlp.ttright.activity;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 
 public class Activity_Base extends FragmentActivity {
@@ -9,6 +10,9 @@ public class Activity_Base extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);//屏幕常亮
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);         //全屏
+
     }
 
 
