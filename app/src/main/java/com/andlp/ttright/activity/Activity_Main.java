e02d9d29e8dac461fee4c7b5c114cb55fc4a042c
liupeng110 <717219917@qq.com>
2017-05-19 16:05:29 +0800

    Initial commit

---
+++ app/src/main/java/com/andlp/ttright/activity/Activity_Main.java
@@ -0,0 +1,15 @@
+package com.andlp.ttright.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.andlp.ttright.R;
+
+public class Activity_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
